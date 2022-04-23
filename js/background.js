01cb8b17bc186fa9dfe3f8c35ee92c1f55ffc787
jaymin-developer <kjmkjm822@naver.com>
2022-04-23 16:38:00 +0900

    Chrome Clone CSS

--- js/background.js
+++ js/background.js
@@ -5,5 +5,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
+bgImage.className = "bgImage";
 
 document.body.appendChild(bgImage);
+console.log(document.body.appendChild(bgImage));
